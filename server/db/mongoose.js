18fafc5071fce28f7e7389a92e4905ebcb0b37a3
Rafael Lagemann <rdlagemann@gmail.com>
2018-01-27 15:10:59 -0200

    app to heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const url = 'mongodb://localhost:27017/TodoApp'
+const url = process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp'
 
 mongoose.Promise = global.Promise // built-in Promise lib
 mongoose.connect(url)
